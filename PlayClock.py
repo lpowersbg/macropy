5d4c186f284cdf3d5a849ca4cfab25a15c074cfb
Liam Power <liam@liampower.net>
2021-10-22 18:11:13 -0400

    Added 2 second sleep for clock hitting 0, removed extraneous text

--- PlayClock.py
+++ PlayClock.py
@@ -2,11 +2,16 @@ import xml.etree.ElementTree as ET
 import time
 import chymac
 
-macro = "PlayClock"
+con = "PlayClockOn"
+coff = "PlayClockOff"
 score = ET.parse("I:\\2019 Sports\\Databases\\Scorehub\\Bug Live Data.xml")
 root = score.getroot()
 
 while(1):
-    clock = root.find('playclock').text)
-    if clock < 10 & clock > 0: chymac.chysend(macro)
+    clock = root.find('playclock').text
+    if clock <= 10 & clock > 0: chymac.chysend(con)
+    if clock <= 0: 
+        time.sleep(2)
+        chymac.chysend(coff)
+    if clock > 10: chymac.chysend(coff)
     time.sleep(1)
