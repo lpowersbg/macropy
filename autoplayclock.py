17972718d84be1581b556f338d110cef5da03d0c
Liam Power <liam@liampower.net>
2022-09-10 21:22:49 -0400

    refinedautoclock

--- autoplayclock.py
+++ autoplayclock.py
@@ -1,30 +1,48 @@
 import xml.etree.ElementTree as xml
 import time
+import datetime
 import chymac
 
 macro1 = "PlayClockIn"
 macro2 = "PlayClockOut"
+thresh = 10
 
 xmlpath = "I:\\2019 Sports\\Databases\\Scorehub\\Bug Live Data.xml"
 xmlpathtest = "C:\\Users\\ChyronHego\\Desktop\\Bug Live Data.xml"
 
 pcin = False
 
+print('Clock-O-Matic 5000 has begun!')
+
+#def timestamp():
+#    dto = datetime.now()
+#    return(dto.strftime("%Y%b%d"))
+
 while(True):
-    stats = xml.parse(xmlpath)
-    root = stats.getroot()
-    rawclock = root[9].text
+    try:
+        stats = xml.parse(xmlpath)
+    except:
+        print('Stats XML failed to open :(')
+    try:
+        root = stats.getroot()
+    except:
+        print('Invalid XML format')
+    try:
+        rawclock = root[9].text
+    except:
+        print('Entry 9 (Playclock) not found. The world weeps.')
     try:
         clock = int(root[9].text)
     #    print(pcin)
         print(clock)
-        if(clock <= 10 and clock > 0 and pcin == False):
+        if(clock <= thresh and clock > 0 and pcin == False):
             chymac.chysend(macro1)
             pcin = True
-        elif(clock == 0 or clock > 10 and pcin == True):
+        elif(clock <= 0 or clock > thresh and pcin == True):
             chymac.chysend(macro2)
             pcin = False
     except:
     #    pass
         print('noclock')
-    time.sleep(1)
+    time.sleep(0.5)
+
