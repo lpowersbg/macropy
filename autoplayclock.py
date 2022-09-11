dc74ccadd18944894365b6a054e5a0c327e46d48
Liam Power <liam@liampower.net>
2022-09-10 21:32:14 -0400

    added timestamps to errors, removed time printing

--- autoplayclock.py
+++ autoplayclock.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as xml
 import time
-import datetime
+from datetime import datetime
 import chymac
 
 macro1 = "PlayClockIn"
@@ -12,29 +12,29 @@ xmlpathtest = "C:\\Users\\ChyronHego\\Desktop\\Bug Live Data.xml"
 
 pcin = False
 
-print('Clock-O-Matic 5000 has begun!')
+def timestamp():
+    dto = datetime.now()
+    return(dto.strftime("[%Y%m%d %H:%M:%S]"))
 
-#def timestamp():
-#    dto = datetime.now()
-#    return(dto.strftime("%Y%b%d"))
+print(timestamp(), 'Clock-O-Matic 5000 has begun!')
 
 while(True):
     try:
         stats = xml.parse(xmlpath)
     except:
-        print('Stats XML failed to open :(')
+        print(timestamp(), 'Stats XML failed to open :(')
     try:
         root = stats.getroot()
     except:
-        print('Invalid XML format')
+        print(timestamp(), 'Invalid XML format')
     try:
         rawclock = root[9].text
     except:
-        print('Entry 9 (Playclock) not found. The world weeps.')
+        print(timestamp(), 'Entry 9 (Playclock) not found. The world weeps.')
     try:
         clock = int(root[9].text)
     #    print(pcin)
-        print(clock)
+    #    print(clock)
         if(clock <= thresh and clock > 0 and pcin == False):
             chymac.chysend(macro1)
             pcin = True
@@ -43,6 +43,6 @@ while(True):
             pcin = False
     except:
     #    pass
-        print('noclock')
+        print(timestamp(), 'No Data in Clock Field')
     time.sleep(0.5)
 
